f58e64ed89b92ab302dc5337d4cf8ff1295c82e1
attila <apatel@aviatrix.com>
2024-07-01 12:28:57 +0100

    entrypoint update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 
-#! /bin/sh
+#!/bin/sh
 
 echo "Hello $1"   # uses the who-to-greet input variable to print "Hello [who-to-greet]" in the log file
 time=$(date)      # gets the current time and sets it as an output variable that actions running later in a job can use.
